12160cc233b2aab2d1c2a08f4b7ac668ba14d042
hyun <dhtmdgussksm@naver.com>
2024-08-11 16:04:10 +0900

    init commit
    init commit

---
+++ yeohappy/yeohappy/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  yeohappy
+//
+//  Created by 오승현 on 8/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("holly,,,,,,")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ yeohappy/yeohappy/yeohappyApp.swift
@@ -0,0 +1,17 @@
+//
+//  yeohappyApp.swift
+//  yeohappy
+//
+//  Created by 오승현 on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct yeohappyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
